2b7d16fb4260fd6c0dde018b6b267bce24a73beb
patryk <relbot23@gmail.com>
2019-12-03 17:26:57 +0100

    Final version

--- main.tf
+++ main.tf
@@ -5,13 +5,7 @@ provider "aws" {
   secret_key = "${var.aws_secret_access_key}"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "terraform-bucket-patryk"
-	key    = "terraform.tfstate"
-	region = "eu-west-3"
-	}
-}
+
 
 resource "aws_instance" "WebServerInstance01" {
   ami           = var.ami
--- s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "terraform-bucket" {
-  bucket = "terraform-bucket-patryk"
-  acl    = "private"
-  region = "eu-west-3"
-  tags = {
-    Name = "Terraform state"
-  }
-}
