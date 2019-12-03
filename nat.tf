876ecf1e2abc8b6e10c221744aac48ff03c57af5
patryk <relbot23@gmail.com>
2019-12-03 16:59:57 +0100

    Nat-GW

--- nat.tf
+++ nat.tf
@@ -1,7 +1,7 @@
 resource "aws_eip" "nat" {
   vpc = true
 }
-resource "aws_nat_gateway" "nat-gw" {
+resource "aws_nat_gateway" "Nat-gw" {
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.main-public-1.id
   depends_on    = [aws_internet_gateway.main-gw]
